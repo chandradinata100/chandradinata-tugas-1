76bbbfbebb5808b3a294dd85cd27357faed70b88
chandradinata100 <113411375+chandradinata100@users.noreply.github.com>
2022-09-13 12:54:54 +0700

    Add files via upload

---
+++ 5 kata c++.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+
+using namespace std;
+
+int main() 
+{
+  int i;
+  for (i = 1; i < 6; i++)
+    cout << "Belajar Coding Di Kelas Algoritma\n";
+  }
---
+++ 5 kata c.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(void) 
+{
+  int i;
+  for (i = 1; i < 6; i++)
+  printf("Belajar Coding Di Kelas Algoritma\n");
+  return 0;
+}
